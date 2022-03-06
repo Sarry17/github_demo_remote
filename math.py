299b47352a7f541da762a6e63db0ed6975fe1c94
sourabh1718 <sourabh_waghule@persistent.com>
2022-03-06 15:04:28 +0530

    Implemented multiply fn on local repo

--- math.py
+++ math.py
@@ -6,7 +6,7 @@ def subtract(x,y):
     return x-y
 # Multiply Implementation
 def multiply(x,y):
-    pass
+    return x*y    # On the local repo
 # Divide Implementation
 def divide(x,y):
     pass
