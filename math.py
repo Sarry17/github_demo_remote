69f8e3fa284f0cf4c5393b747e117cf879e6b632
sourabh1718 <sourabh_waghule@persistent.com>
2022-03-06 14:33:20 +0530

    Implemented subtract fn

--- math.py
+++ math.py
@@ -3,7 +3,7 @@ def add(x,y):
     return x+y
 #Subtract Implementation
 def subtract(x,y):
-    pass
+    return x-y      # On the Bug456 branch
 # Multiply Implementation
 def multiply(x,y):
     pass
